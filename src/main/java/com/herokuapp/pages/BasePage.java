535ea706cc628804b27e47ebad89cbd8a1303908
cristina <cristina.ilea2@yahoo.com>
2024-02-05 20:04:14 +0200

    varianta 2

--- src/main/java/com/herokuapp/pages/BasePage.java
+++ src/main/java/com/herokuapp/pages/BasePage.java
@@ -1,2 +1,15 @@
-package com.herokuapp.pages;public class BasePage {
+package com.herokuapp.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected static String BASE_URL = "https://the-internet.herokuapp.com/";
+
+    public BasePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
--- src/main/java/com/herokuapp/pages/SecureAreaPage.java
+++ src/main/java/com/herokuapp/pages/SecureAreaPage.java
@@ -1,2 +1,26 @@
-package com.herokuapp.pages;public class SecureAreaPage {
+package com.herokuapp.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SecureAreaPage extends BasePage {
+
+    protected static String SECUREAREA_URL = BASE_URL+"secure";
+    public SecureAreaPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id="flash") private WebElement successMessage;
+    @FindBy(linkText = "Logout") private WebElement logoutButton;
+
+    public String getSuccessMessageText(){
+        return successMessage.getText();
+    }
+
+    public boolean isLogoutButtonDisplayed(){
+        return logoutButton.isDisplayed();
+    }
+
+
 }
